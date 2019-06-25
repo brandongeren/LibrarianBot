56eeadc751e86baae6d752ac54e4495e0da6e546
Brandon Geren <brandongeren@gmail.com>
2019-06-25 17:51:55 -0500

    Add command for sharing github info

--- modules/commands.js
+++ modules/commands.js
@@ -28,6 +28,25 @@ function findCommands(msg) {
       msg.channel.send('Error: could not find card ' + query);
     }
   }
+
+  if (msg.content === '!github') {
+    // test embed here:
+    // https://leovoel.github.io/embed-visualizer/
+    let embed = {
+      "embed": {
+        "description": "**See a bug?**\n[Open an issue](https://github.com/brandongeren/SeerBot).\n\n**Want to contribute?**\n[Make a pull request](https://github.com/brandongeren/SeerBot)\n\n**Just want to see the source code?**\n[Click here](https://github.com/brandongeren/SeerBot)\n\n**Want to help develop a new dueling sim?**\nReach out to me via Discord message\nAnarchist Duelist#0166",
+        "color": 16711680,
+        "image": {
+          "url": "https://avatars0.githubusercontent.com/u/9919?s=280&v=4"
+        },
+        "author": {
+          "name": "Anarchist Duelist",
+          "url": "https://github.com/brandongeren",
+          "icon_url": "https://cdn.discordapp.com/avatars/379361995779997717/2766136dd6a7b5ebd01435aa7794aaa3.png?size=256"
+        }
+      }
+    }
+  msg.channel.sendEmbed(embed);
 }
 
 function regexEscape(str) {
