bf9bc86fcbcb4e55f01f7a865e4624d2632c5da1
Brandon Geren <brandongeren@gmail.com>
2019-06-25 10:48:25 -0500

    Card database search working

---
+++ bot.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const FuzzySet = require('fuzzyset.js');
+
+let cardInfo = {}
+// might have to update the path for cards.json;
+let cards = JSON.parse(fs.readFileSync('cards.json'))[0];
+let cardSet = FuzzySet([], false);
+
+for (let card of cards) {
+  cardName = card.name.toLowerCase();
+  cardSet.add(cardName);
+  cardInfo[cardName] = card;
+}
+
+function findCard(input) {
+  let name = input.toLowerCase();
+  let match = cardSet.get(name);
+  if (match) {
+    cardName = match[0][1];
+    console.log(cardInfo[cardName]);
+  }
+  else {
+    console.log('fuck you');
+  }
+}
