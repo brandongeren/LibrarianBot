f722240d6348357bcf5336655ed237997d915fc9
Brandon Geren <brandongeren@gmail.com>
2019-07-23 22:31:38 -0500

    Grab the latest cards on bot startup

--- modules/findCards.js
+++ modules/findCards.js
@@ -1,9 +1,16 @@
 const FuzzySet = require('fuzzyset.js');
-const fs = require('fs');
+// const fs = require('fs');
+var request = require('sync-request');
 
 let cardInfo = {}
 // might have to update the path for cards.json;
-let cards = JSON.parse(fs.readFileSync('./data/cards.json'))[0];
+
+let cardsRaw = request('GET', 'https://db.ygoprodeck.com/api/v4/cardinfo.php');
+console.log(cardsRaw.getBody());
+
+let cards = JSON.parse(cardsRaw.getBody())[0];
+
+// cards = JSON.parse(fs.readFileSync('./data/cards.json'))[0];
 let cardSet = FuzzySet([], false);
 // cardSetStrict is used when we're not very sure of the card that comes up as a match
 // in that case, we check another database, which works better for substrings
@@ -99,6 +106,16 @@ for (let card of cards) {
   cardSetStrict.add(cardName);
   cardInfo[cardName] = card;
 }
+
+// async function downloadCards() {
+//   let cardsRaw = '';
+//   await https.get('https://db.ygoprodeck.com/api/v4/cardinfo.php', (res) => {
+//     res.on('data', (chunk) => {
+//         cardsRaw += chunk;
+//     });
+//   });
+//   return cardsRaw;
+// }
 
 function findCard(input) {
   let name = input.toLowerCase();
